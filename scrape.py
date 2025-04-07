8b1498fef84f6ab2c2def6c2491b3bd1947c9f76
Jon Moss <me@jonathanmoss.me>
2025-04-06 21:18:51 -0400

    Switch from JSON to CSV

--- scrape.py
+++ scrape.py
@@ -1,21 +1,20 @@
 import json
+import pandas as pd
 import requests
 
 def get_items(item_class, page):
-  req = requests.get("https://api.doge.gov/savings/%s?sort_by=savings&sort_order=desc&per_page=100&page=%i" % (item_class, page))
+  req = requests.get("https://api.doge.gov/savings/%s?sort_by=savings&sort_order=desc&per_page=500&page=%i" % (item_class, page))
   req = req.json()
   return [req['result'][item_class], req['meta']['pages']]
 
 def save_items(item_class):
-  results = []
   first_page = get_items(item_class, 1)
-  results.extend(first_page[0])
+  dfs = [pd.DataFrame(first_page[0])]
 
   for page in range(2, first_page[1] + 1):
-    results.extend(get_items(item_class, page)[0])
+    dfs.append(pd.DataFrame(get_items(item_class, page)[0]))
 
-  with open("data/%s.json" % (item_class), "w") as f:
-    json.dump(results, f, indent=2)
+  pd.concat(dfs).to_csv("data/%s.csv" % (item_class), index=False)
 
 for item_class in ["contracts", "grants", "leases"]:
   save_items(item_class)
